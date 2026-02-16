7b583b9fac91acde82724d390011e3a08f7b8597
Sophie Kane <sophie.kane@protonmail.com>
2026-02-16 17:45:03 +0000

    AAC-01: initialise Spring Boot API skeleton

---
+++ src/main/java/com/sophie/aac/AacApiApplication.java
@@ -0,0 +1,13 @@
+package com.sophie.aac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AacApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AacApiApplication.class, args);
+	}
+
+}
