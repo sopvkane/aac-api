36edf49416d68fb98546c84061c0af5428beb0ec
Sophie Kane <sophie.kane@protonmail.com>
2026-02-17 15:11:41 +0000

    AAC-04: add phrases CRUD with Flyway + Postgres and integration tests

--- src/test/java/com/sophie/aac/AacApiApplicationTests.java
+++ src/test/java/com/sophie/aac/AacApiApplicationTests.java
@@ -1,36 +1,10 @@
 package com.sophie.aac;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.NONE,
-    classes = AacApiApplication.class,
-    properties = {
-        "spring.main.web-application-type=none",
-
-        // IMPORTANT: correct key is spring.autoconfigure.exclude (NOT spring.autoconfigure.exclude= in code)
-        "spring.autoconfigure.exclude="
-            + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-            + "org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration,"
-            + "org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration,"
-            + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
-            + "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration,"
-            + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration",
-
-        // Extra safety: even if something sneaks in, don't run migrations
-        "spring.flyway.enabled=false",
-
-        // Extra safety: avoid any attempt at schema actions
-        "spring.jpa.hibernate.ddl-auto=none"
-    }
-)
-@ActiveProfiles("test")
+@Disabled("Redundant: integration tests boot the app with Testcontainers")
 class AacApiApplicationTests {
-
-  @Test
-  void contextLoads() {
-    // CI-safe: verifies Spring context loads without needing Postgres on localhost.
-  }
+  @Test void contextLoads() {}
 }
+
