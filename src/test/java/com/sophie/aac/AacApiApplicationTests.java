c1f2b175e677858795344fadb0f0e6a451869a8c
Sophie Kane <sophie.kane@protonmail.com>
2026-02-17 13:15:16 +0000

    AAC-04: make contextLoads CI-safe (no DB required)

--- src/test/java/com/sophie/aac/AacApiApplicationTests.java
+++ src/test/java/com/sophie/aac/AacApiApplicationTests.java
@@ -3,18 +3,17 @@ package com.sophie.aac;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-    properties = {
-        "spring.autoconfigure.exclude=" +
-            "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-            "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
-    }
-)
+@SpringBootTest(properties = {
+    // CI-safe: don't try to wire a datasource / JPA / Flyway for this smoke test
+    "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class AacApiApplicationTests {
 
   @Test
   void contextLoads() {
-    // verifies Spring context boots without DB wiring
+    // Intentionally empty: proves Spring context starts without DB dependency.
   }
 }
